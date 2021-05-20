5c20e50490416c78be1fa2ec1f08680fabc615b8
Yupinnu <83113114+Yupinnu@users.noreply.github.com>
2021-05-20 23:08:40 +0700

    finish

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is a test</h1>
+      <p>I love food.</p>
+    </div>
+  );
+}
+
+export default Note;
